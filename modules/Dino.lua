af66ed451ae30f48306a33b5e313294c220727f1
Dino-V <Dino-V@users.noreply.github.com>
2016-09-18 22:17:19 -0700

    test

--- modules/Dino.lua
+++ modules/Dino.lua
@@ -1,10 +1,15 @@
 -- Todo: Work on c00l stuff!!1
--- Some sort of simulator idea like the luaC thing but my own modified made up version?
+-- Some interesting stuff going down here..
 
 
 local module = {}
 
-local states = {}
+-- {name, level}
+local admins = {}
+
+local function addAdmin(name, lvl)
+  table.insert(admins, {name, lvl})
+end
 
 
 return module
