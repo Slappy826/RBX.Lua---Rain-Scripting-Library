db0b2d5b44df95931eaf9f59835a89ac8b3a7908
Dino-V <Dino-V@users.noreply.github.com>
2016-09-18 22:14:25 -0700

    Update Dino.lua

--- modules/Dino.lua
+++ modules/Dino.lua
@@ -1,5 +1,10 @@
 -- Todo: Work on c00l stuff!!1
+-- Some sort of simulator idea like the luaC thing but my own modified made up version?
+
 
 local module = {}
+
+local states = {}
+
 
 return module
